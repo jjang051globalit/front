25983c9172374182787d2a986963c1afaad2dbaa
jjang051globalit <jjang051globalit@gmail.com>
2023-05-17 16:25:23 +0900

    js basic

---
+++ hwenc/js/hello.js
@@ -0,0 +1,18 @@
+let myName = "장성호";
+const pi = 3.14;
+var num = 10;
+myName = 10;
+console.log(10 + 10);
+console.log(100 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 3);
+for (var i = 0; i < 10; i++) {
+  console.log("hello js");
+}
+console.log(i);
+function add(a, b) {
+  return a + b;
+}
+const result = add(10, 20);
+console.log(result);
