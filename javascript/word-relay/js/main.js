677a8f772c6e111c19f7def5927714842ecdcea6
jjang051globalit <jjang051globalit@gmail.com>
2023-05-26 15:22:40 +0900

    api 호출

--- javascript/word-relay/js/main.js
+++ javascript/word-relay/js/main.js
@@ -2,29 +2,49 @@ const word = document.querySelector(".word");
 const wordList = document.querySelector(".word-list ul");
 //1. 주어진 단어를 하나 만든다.  배열에다 값을 넣고 그 중에 하나 골라서 뿌리기...
 //console.log("🚀 ~ file: main.js:2 ~ word:", word);
-const firstWords = ["강아지", "소나기", "기상청", "청소부", "부잣집"];
-const firstWord = firstWords[Math.floor(Math.random() * 5)];
+const wordArray = []; // 배열은 참조 reference
+const firstWords = ["강아지", "소나기", "기상청", "청소부", "부잣집", "호랑이"];
+const firstWord = firstWords[Math.floor(Math.random() * firstWords.length)];
 
+wordArray.push(firstWord);
 const appedWord = (inputWord) => {
   const li = document.createElement("li");
   li.textContent = inputWord;
   wordList.append(li);
+  wordArray.push(inputWord);
 };
+
+const fault = () => {
+  word.value = "";
+  gsap.from(".input-box", { x: 100, duration: 1, ease: "elastic.out(1, 0.2)" });
+};
+
 appedWord(firstWord);
 //2. word에 글자를 입력하고 enter쳤을때 마지막 단어의 마지막 글자랑 입력한 단어의 첫글자가 같은지 따져야함
 word.addEventListener("keyup", (e) => {
   if (e.key === "Enter" || e.keyCode === 13) {
     if (word.value.length !== 3) {
-      word.value = "";
+      fault();
       return;
     }
     const lastWord = document.querySelector(".word-list ul li:last-child").textContent;
     const lastChar = lastWord.substring(2);
     if (word.value.substring(0, 1) !== lastChar) {
-      word.value = "";
+      fault();
+      return;
+    }
+    if (wordArray.includes(word.value)) {
+      fault();
       return;
     }
     appedWord(word.value);
     word.value = "";
   }
 });
+fetch("https://opendict.korean.go.kr/api/search?key=9A816277AA70D1F8EC1ADC87D52CA5E2&q=기럭키&req_type=json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
