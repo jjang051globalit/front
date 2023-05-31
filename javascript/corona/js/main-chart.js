78ea94681431573895a8d95a6cb782f4f83700c6
jjang051globalit <jjang051globalit@gmail.com>
2023-05-31 15:18:36 +0900

    chart

--- javascript/corona/js/main-chart.js
+++ javascript/corona/js/main-chart.js
@@ -41,30 +41,25 @@ const datePicker = new Lightpick({
   },
 });
 datePicker.setDate(new Date());
-//loadCoronaData("2023-05-30");
+const ctx = document.querySelector(".chart");
 
-const numbers = [2, 1, 4, 3, 44, 5, 11, 88, 99, 4, 6, 78, 54, 34, 15];
-console.log(numbers.sort((a, b) => a - b));
-//console.log(numbers.sort((a, b) => b - a));
-const chars = ["다", "나", "가", "라", "나라"];
-
-const sortedChars = chars.sort((a, b) => {
-  if (a > b) return -1;
-  if (a === b) return 0;
-  if (a < b) return 1;
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["대구", "인천", "서울", "경기", "세종", "제주"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [300, 250, 800, 1200, 150, 80],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
 });
-console.log(chars[2]);
-
-const animals = ["chicken", "cat", "puppy"];
-const myAnimals = [...animals];
-// 배열 또는 객체의 원본을 훼손하지 않고 복사해갈때 많이 사용을 한다.
-animals.push("camel");
-console.log(myAnimals);
-let a = 100;
-let b = a;
-a += 100;
-console.log(b + "===" + a);
-const person = { name: "장성호", age: 30 };
-const person02 = { nickName: "마동석", ...person }; // ... spread operator
-person.name = "장동건";
-console.log(person02); // 얕은 복사 (shallow copy)
