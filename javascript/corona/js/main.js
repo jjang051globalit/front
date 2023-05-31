2c746d8a4fa1074e043fb05b7aa69e4f8ece583c
jjang051globalit <jjang051globalit@gmail.com>
2023-05-31 12:13:57 +0900

    loading

--- javascript/corona/js/main.js
+++ javascript/corona/js/main.js
@@ -1,11 +1,21 @@
+const ldsEllipsis = document.querySelector(".lds-ellipsis");
 const coronaList = document.querySelector(".coronaList ul");
+const removeItem = () => {
+  const imgItem = document.querySelectorAll(".coronaList ul li");
+  imgItem.forEach((item, idx) => {
+    item.remove();
+  });
+};
 const loadCoronaData = async (date) => {
+  removeItem();
+  ldsEllipsis.classList.remove("off");
   const corona = await fetch(
     `http://apis.data.go.kr/1352000/ODMS_COVID_04/callCovid04Api?serviceKey=Wnus4QpirWGI56CfvzMWDIDHMRL%2FmEF%2FqTl9gwVNbRggLYTGPFIdwBy0L51B%2B27d5QRbLanNmIAxPwNvl7dKPA%3D%3D&pageNo=1&numOfRows=500&apiType=JSON&std_day=${date}`
   );
   const response = await corona.json();
   //.then((response) => response.json())
   const items = response.items;
+  ldsEllipsis.classList.add("off");
   items.forEach((item, idx) => {
     console.log(item.gubun + "===" + item.incDec);
     const li = document.createElement("li");
@@ -19,6 +29,7 @@ const loadCoronaData = async (date) => {
     li.append(incDec);
     coronaList.append(li);
   });
+  gsap.from(".coronaList ul li", { scale: 0, duration: 0.5, stagger: 0.02 });
 };
 const datePicker = new Lightpick({
   field: document.querySelector(".date-picker"),
